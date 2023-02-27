3ad0cc62001178668e4c86e2745f47e5025ba313
DrizzyJ56 <blevins.j921@gmail.com>
2023-02-27 14:05:15 -0500

    first commit/ made server

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const morgan = require('morgan')
 const app = express();
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
 const PORT = 1337;
 
+
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
 });
+
+app.use(morgan("dev"))
