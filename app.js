97233abfed696c1ce202313f04b84321981a6e85
Cesar bacaro-ley <cesarbacaro-ley@Cesars-MacBook-Pro.local>
2023-02-27 13:32:14 -0600

    sec commit

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 const express = require("express");
-const morgan = require('morgan')
+const morgan = require("morgan");
+const postBank = require("./postBank");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => {
+  const posts = postBank.list();
+
+  const html = `<!DOCTYPE html>
+  <html>
+  <head>
+  <title>Wizard News</title>
+  </head>
+  <body>
+    <ul>
+    ${posts.map((post) => `<li>${post.title}</li>`)}
+    </ul>
+  </body>
+  </html>`;
+});
 
 const PORT = 1337;
-
 
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
 });
 
-app.use(morgan("dev"))
+app.use(morgan("dev"));
